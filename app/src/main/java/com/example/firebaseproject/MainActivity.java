5bf879996ec3a2d9ad0cdb3a272c949081bdd7cc
t1meclock <kchekalova00@mail.ru>
2022-03-10 23:37:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.firebaseproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edtPhone = findViewById(R.id.phone);
+        EditText edtEmail = findViewById(R.id.email);
+        EditText edtPass = findViewById(R.id.password);
+        Button btn = findViewById(R.id.signup);
+
+        btn.setOnClickListener(view -> {
+            String phone = edtPhone.getText().toString();
+            String email = edtEmail.getText().toString();
+            String password = edtPass.getText().toString();
+
+            database = FirebaseDatabase.getInstance("https://fireeeee-5dab3-default-rtdb.firebaseio.com/")
+                    .getReference().child("Users");
+
+            Users user = new Users(phone, email, password);
+            database.child(phone).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    edtPhone.getText().clear();
+                    edtEmail.getText().clear();
+                    edtPass.getText().clear();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), "Ошибка", Toast.LENGTH_LONG).show();
+                }
+            });
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firebaseproject/Users.java
@@ -0,0 +1,13 @@
+package com.example.firebaseproject;
+
+public class Users {
+    public String phone;
+    public String email;
+    public String password;
+
+    public Users(String phone, String email, String password){
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+}
